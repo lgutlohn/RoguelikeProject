884aa9bad2764dd38b1cc1a1c853b8c57225eb7b
Benjamin Clarke <bac351@nyu.edu>
2014-03-10 12:26:00 -0400

    no message

---
+++ Assets/Scripts/CamControls.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControls : MonoBehaviour {
+	
+	public float turnSpeed = 10f;
+	public float forwardSpeed = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+	}
+	void FixedUpdate(){
+		
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate (Vector3.left* forwardSpeed);
+			
+
+		}
+		if(Input.GetKey(KeyCode.UpArrow)){
+			transform.Translate (Vector3.up* forwardSpeed);
+		}
+				
+		if(Input.GetKey(KeyCode.DownArrow)){
+			transform.Translate (Vector3.down* forwardSpeed);
+		}
+					
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate (Vector3.right * forwardSpeed);
+		}
+		
+		if(Input.GetKey(KeyCode.Comma)){
+			transform.Translate (Vector3.back* forwardSpeed);
+		}
+		
+		if(Input.GetKey(KeyCode.Period)){
+			transform.Translate (Vector3.forward* forwardSpeed);
+		}
+
+
+	}
+}
